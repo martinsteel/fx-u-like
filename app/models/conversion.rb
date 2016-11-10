2649e4011bb3a52e77f207a093b11476518de5f1
Martin Steel <martin@martinsteel.co.uk>
2016-11-10 16:35:30 +0000

    Date range limits

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -2,11 +2,14 @@ require 'date'
 
 class Conversion
     attr_accessor :date, :amount, :from, :to
+    attr_reader :min_date, :max_date
 
     def initialize(date = Date.today, amount = 0, from = "GBP", to = "GBP")
         @date = date
         @amount = amount
         @from = from
         @to = to
+        @min_date = ExchangeRate.minimum(:date)
+        @max_date = ExchangeRate.maximum(:date)
     end
 end
